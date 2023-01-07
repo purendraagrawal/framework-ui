60a9f3ea0b1b37150b7e3b55cd3f5456d990584b
Purendra Agrawal <purendra.agrawal@gmail.com>
2023-01-07 13:03:59 +0530

    Update the path of properties file

--- src/main/java/personal/purendra/utils/ReadProperties.java
+++ src/main/java/personal/purendra/utils/ReadProperties.java
@@ -13,7 +13,7 @@ public class ReadProperties {
   private ReadProperties() {
     prop = new Properties();
     try (FileInputStream fileInput =
-        new FileInputStream(new File("src/test/resources/data1.properties"))) {
+        new FileInputStream(new File("src/test/resources/data.properties"))) {
       prop.load(fileInput);
     } catch (IOException e) {
       throw new FrameworkException(e.getMessage(),e.getCause());
